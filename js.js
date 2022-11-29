7128513d30f32a54ee185a75325ffd15c1bf4cf2
SpbJulia <Start.job.juli@gmail.com>
2022-11-29 19:56:14 +0300

    Задачка
    1

--- js.js
+++ js.js
@@ -1,11 +1,53 @@
 alert ("Привет!")
 confirm ("Вы человек?")
-let number = prompt ("Введите  3х значное число:")
-let units = number%10
-let a = (number/10) - (units*0.1)
-let dozens = a%10
-let hundreds = (a/10) - (dozens*0.1)
+// let number = prompt ("Введите  3х значное число:")
+// let units = number%10
+// let a = (number/10) - (units*0.1)
+// let dozens = a%10
+// let hundreds = (a/10) - (dozens*0.1)
 
-alert ("В введённом числе количество сотен равно: " + hundreds)
-alert ("В введённом числе количество десятков равно: " + dozens)
-alert ("В введённом числе количество едениц равно: " + units)
+// alert ("В введённом числе количество сотен равно: " + hundreds)
+// alert ("В введённом числе количество десятков равно: " + dozens)
+// alert ("В введённом числе количество едениц равно: " + units)
+
+// let num1 = +prompt()
+// let num2 = +prompt()
+// let num3 = +prompt()
+// let num4 = +prompt()
+// let num5 = +prompt()
+// let num6 = +prompt()
+// let num7 = +prompt()
+
+// let result = num1 + num2
+// let result2 = num2 + num4
+// let result3 = num5 + num6
+
+//alert(`Наш результат: ${result}Наш результат: ${result2} Наш результат: ${result3}`)
+
+// let name1 = prompt ("Как тебя зовут?")
+// alert (`Классное имя: ${name1}`)
+
+// let name1 = prompt ("Как тебя зовут?")
+// let name2 = prompt ("Сколько тебе лет?")
+
+// alert (`Тебя зовут: ${name1}, тебе ${name2}`)
+
+
+
+//alert(`result ${+`stroka`}`)
+
+//== нестрогое сравенине
+//===строгое сравнение
+
+
+let nun1 = prompt ("Введите первое число")
+let nun2 = prompt ("Введите второе число")
+
+if (nun1>nun2){
+    alert(`первое число ${nun1} больше второго ${nun2}`)
+}else {
+
+}alert(`первое число ${nun1} меньше второго ${nun2}`)
+
+
+
